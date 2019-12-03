864c75f8551873ab684cb3938eacfe55ca6dbc2f
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-12-03 13:36:03 -0500

    Fail.

---
+++ hypotenuse.js
@@ -0,0 +1,16 @@
+const readlineSync = require ("require-sync");
+
+const amount =(readlineSync.question("Enter base: "));
+const amount2 =(readlineSync.question("Enter height: "));
+const amount3 =(readlineSync.question("Enter hypotenuse: "));
+
+const base = amount
+const height = amount2
+const hypotenuse = amount3
+
+if
+  const righttriangle = base ** * height ** = hypotenuse **;
+  console.log("\nYes, that's a right triangle!");
+}else {
+  console.log("\nNope...not a right triangle.")
+}
